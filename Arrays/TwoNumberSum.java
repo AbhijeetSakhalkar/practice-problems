aa60c1fa279c6f7a488fc8db685f1fca6f7b2659
Abhijeet Sakhalkar <sakhalkar123abj@gmail.com>
2024-07-02 19:07:01 +0530

    first commit

---
+++ Arrays/TwoNumberSum.java
@@ -0,0 +1,12 @@
+public class TwoNumberSum {
+    public static void main (String args) {
+
+    }
+
+    public static int[] twoNumberSum(int[] array, int targetSum) {
+
+        return new int[0];
+    }
+
+}
+
