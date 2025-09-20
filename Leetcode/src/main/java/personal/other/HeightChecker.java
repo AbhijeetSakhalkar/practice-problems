3cee66d7eec4cb286ec5668c98e3e4d149edd808
Abhijeet Sakhalkar <sakhalkar123abj@gmail.com>
2025-09-20 11:18:18 +0530

    Week4

---
+++ Leetcode/src/main/java/personal/other/HeightChecker.java
@@ -0,0 +1,25 @@
+package personal.other;
+
+
+import java.util.Arrays;
+
+public class HeightChecker {
+
+    public static void main(String[] args) {
+        HeightChecker h = new HeightChecker();
+        int ans = h.heightChecker(new int []{1,1,4,2,1,3});
+        System.out.println(ans);
+    }
+
+    public int heightChecker(int[] heights) {
+        int [] newHeight = Arrays.copyOf(heights, heights.length);
+        Arrays.sort(newHeight);
+        int ans = 0;
+        for (int i = 0 ; i  < heights.length ; i++) {
+            if(heights[i] != newHeight[i]) {
+                ans ++;
+            }
+        }
+        return ans;
+    }
+}
