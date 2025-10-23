6d227e29bae5eaaecc934eff938f0f17a295c2d6
Abhijeet Sakhalkar <sakhalkar123abj@gmail.com>
2025-10-23 12:48:57 +0530

    Week9

---
+++ Leetcode/src/main/java/personal/dg/arrays/ContainsDuplicate.java
@@ -0,0 +1,23 @@
+package personal.dg.arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        ContainsDuplicate c = new ContainsDuplicate();
+        boolean b = c.containsDuplicate(new int[]{1,2,3,4});
+        System.out.println(b);
+    }
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i : nums) {
+            if (set.contains(i)) {
+                return true;
+            } else {
+                set.add(i);
+            }
+        }
+        return false;
+    }
+}
---
+++ Leetcode/src/main/java/personal/dg/arrays/LeftRightSumDifferences.java
@@ -0,0 +1,26 @@
+package personal.dg.arrays;
+
+import java.util.Arrays;
+
+public class LeftRightSumDifferences {
+    public static void main(String[] args) {
+        LeftRightSumDifferences l = new LeftRightSumDifferences();
+        int [] ans = l.findDifferenceArray(new int [] {2, 5, 1, 6, 1});
+        System.out.println(Arrays.toString(ans));
+    }
+    public int[] findDifferenceArray(int[] nums) {
+        int n = nums.length;
+        int[] differenceArray = new int[n];
+        int r=0;
+        int l=0;
+        for (int i = 0 ; i < n ; i ++) {
+            r = r + nums[i];
+        }
+        for (int i = 0 ; i < n ; i ++) {
+            r = r - nums[i];
+            differenceArray[i] = Math.abs(r - l);
+            l = l + nums[i];
+        }
+        return differenceArray;
+    }
+}
