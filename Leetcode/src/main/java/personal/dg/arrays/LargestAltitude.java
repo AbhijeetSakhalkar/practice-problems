559acdd4b4c4320c1579aa99253222f9d9b3825e
Abhijeet Sakhalkar <sakhalkar123abj@gmail.com>
2025-10-25 20:59:20 +0530

    Week9

---
+++ Leetcode/src/main/java/personal/dg/arrays/LargestAltitude.java
@@ -0,0 +1,20 @@
+package personal.dg.arrays;
+
+public class LargestAltitude {
+    public static void main(String[] args) {
+        LargestAltitude l = new LargestAltitude();
+        int ans = l.largestAltitude(new int[]{4, -3, 2, -1, -2});
+        System.out.println(ans);
+    }
+    public int largestAltitude(int[] gain) {
+        int maxAltitude = 0; // To store the maximum altitude encountered
+        int sum = 0;
+        for (int i = 0 ; i < gain.length ; i ++) {
+            sum += gain[i];
+            if (sum > maxAltitude) {
+                maxAltitude = sum;
+            }
+        }
+        return maxAltitude;
+    }
+}
---
+++ Leetcode/src/main/java/personal/dg/matrix/RichestCustomer.java
@@ -0,0 +1,23 @@
+package personal.dg.matrix;
+
+public class RichestCustomer {
+    public static void main(String[] args) {
+        int ans =  maximumWealth(new int [][]{
+                {5, 10, 15},
+                {10, 20, 30},
+                {15, 30, 45}
+        });
+        System.out.println(ans);
+    }
+    public static int maximumWealth(int[][] accounts) {
+        int maxWealth = 0;
+        for (int[] account : accounts) {
+            int custWealth = 0;
+            for (int i : account) {
+                custWealth += i;
+            }
+            maxWealth = Math.max(maxWealth, custWealth);
+        }
+        return maxWealth;
+    }
+}
