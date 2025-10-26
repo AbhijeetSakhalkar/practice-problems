09699a350e288f606a721f25623f5b6264a8f3da
Abhijeet Sakhalkar <sakhalkar123abj@gmail.com>
2025-10-26 18:45:28 +0530

    Week9

---
+++ Leetcode/src/main/java/personal/dg/matrix/DiagonalSum.java
@@ -0,0 +1,26 @@
+package personal.dg.matrix;
+
+public class DiagonalSum {
+    public static void main(String[] args) {
+        int ans = diagonalSum(new int [][] {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        });
+        System.out.println(ans);
+    }
+    public static int diagonalSum(int[][] mat) {
+        int totalSum = 0;  // Initialize the total sum
+        for (int i=0,j=0; i<mat.length; i++,j++) {
+            totalSum += mat[i][j];
+        }
+        for(int i=0,j= mat.length-1;i< mat.length; i++,j--) {
+            totalSum += mat[i][j];
+        }
+        if (mat.length%2 != 0) {
+            int mid = mat.length/2;
+            totalSum = totalSum - mat[mid][mid];
+        }
+        return totalSum;  // Return the calculated total sum
+    }
+}
---
+++ Leetcode/src/main/java/personal/dg/matrix/MaxOnes.java
@@ -0,0 +1,19 @@
+package personal.dg.matrix;
+
+public class MaxOnes {
+    public int[] findMaxOnesRow(int[][] mat) {
+        int maxOnesIdx = 0;
+        int maxOnesCount = 0;
+        for (int i = 0 ; i < mat.length; i++) {
+            int count = 0;
+            for(int j = 0 ; j < mat[i].length; j++) {
+                if (mat[i][j] == 1) count ++;
+            }
+            if (count > maxOnesCount) {
+                maxOnesCount = count;
+                maxOnesIdx = i;
+            }
+        }
+        return new int[]{maxOnesIdx, maxOnesCount};
+    }
+}
