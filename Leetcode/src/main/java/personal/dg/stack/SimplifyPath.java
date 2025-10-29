5822cf6814289cc88016b834aed36110fbc33498
Abhijeet Sakhalkar <sakhalkar123abj@gmail.com>
2025-10-29 22:19:57 +0530

    Week10

---
+++ Leetcode/src/main/java/personal/dg/stack/SimplifyPath.java
@@ -0,0 +1,32 @@
+package personal.dg.stack;
+
+import java.util.Stack;
+
+public class SimplifyPath {
+
+    public static void main(String[] args) {
+        SimplifyPath s = new SimplifyPath();
+        String ans = s.simplifyPath("/a//b////c/d//././/..");
+        System.out.println(ans);
+    }
+
+    public String simplifyPath(String path) {
+        String [] arr = path.split("/");
+        Stack<String> stack = new Stack<>();
+        for(String s : arr) {
+            if (s.equals("/") || s.equals(".")|| s.isEmpty()) {}
+            else if (s.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(s);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String s : stack) {
+            sb.append("/").append(s);
+        }
+        return "/" + String.join("/", stack);
+    }
+}
