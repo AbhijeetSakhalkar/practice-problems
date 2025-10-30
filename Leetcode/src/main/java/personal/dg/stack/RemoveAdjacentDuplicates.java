e1436d53e222a11fe06844973dcf9b9e1d67e1b2
Abhijeet Sakhalkar <sakhalkar123abj@gmail.com>
2025-10-30 20:29:26 +0530

    Week10

---
+++ Leetcode/src/main/java/personal/dg/stack/RemoveAdjacentDuplicates.java
@@ -0,0 +1,26 @@
+package personal.dg.stack;
+
+import java.util.Stack;
+
+public class RemoveAdjacentDuplicates {
+    public static void main(String[] args) {
+        RemoveAdjacentDuplicates r = new RemoveAdjacentDuplicates();
+        String ans = r.removeDuplicates("abbaaca");
+        System.out.println(ans);
+    }
+    public String removeDuplicates(String str) {
+        Stack<Character> stack = new Stack<>();
+        for(Character c : str.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek().equals(c)) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        StringBuffer sb = new StringBuffer();
+        for (Character s : stack) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+}
